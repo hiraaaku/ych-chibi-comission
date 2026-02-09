5d5c7d637e7c308f97e497b8580dcd72fa1eb57d
Audrey Etantyo <audreyetantyo.w@gmail.com>
2026-02-09 16:49:50 +0700

    Update Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/yuuepye-favicon.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/yuuepye-favicon.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
